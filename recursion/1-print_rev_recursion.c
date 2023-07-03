cb4346ebde6616fce79132392ddd44fd14b244d3
Alexander1455 <alexander21111121@gmail.com>
2023-07-02 19:52:25 -0700

    code correction and new comments

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Imprime una cadena seguida de una nueva línea
- * @s: cadena
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: string
  *
- * Return: En caso de éxito, devuelve 1.
- * En caso de error, se devuelve -1 y se establece errno correctamente.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	/* Si el primer carácter de la cadena es NULL (fin de cadena) */
-	if (*s == 0)
-	{
-		/* Imprime un salto de línea */
-		_putchar('\n');
-		/* Finaliza la función */
-		return;
-	}
-	/* Imprime el carácter actual */
-	_putchar(*s);
-/*Llama recursivamente a la función con el siguiente carácter de la cadena*/
-	_puts_recursion(s + 1);
-}
+    if (*s != '\0')/*Comprueba si el caracter actual no es el terminador nulo*/
+    {
+        _print_rev_recursion(s + 1);
+	/*Llama a la función recursivamente pasando la siguiente posición en la cadena*/
+        _putchar(*s); /*Imprime el caracter actual*/
+    }
+}}
