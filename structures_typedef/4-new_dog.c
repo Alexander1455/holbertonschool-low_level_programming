519df988769afb3daa037ee0c535451edebbb676
Alexander1455 <alexander21111121@gmail.com>
2023-07-08 16:14:56 -0700

    Returns pointer to newly allocated space which contains a string

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,48 +2,52 @@
 #include <stdlib.h>
 
 /**
- * new_dog - create a new dog
- * @name: char string name
- * @age: int age
- * @owner: char string owner
- * Return: pointer to new dog
+ * _strdup - Returns pointer to newly allocated space which contains a string
+ * @str: String to be copied
+ *
+ * Return: Pointer to new string identical to str, NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+	int len;
+
+	if (!str)
+		return (NULL);
+	while (*(str + i))
+		++i;
+	len = i;
+	s = malloc(sizeof(char) * (len + 1));
+	if (!s)
+		return (NULL);
+	for (i = 0; i <= len; ++i)
+		*(s + i) = *(str + i);
+	return (s);
+}
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Return: Created dog, NULL if fail
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *doge;
-	int i, j, k;
-	char *nom, *dueño;
+	dog_t *d;
 
-	doge = malloc(sizeof(struct dog));
-	if (doge == NULL)
+	if (!name || !owner)
 		return (NULL);
-
-	for (i = 0; name[i] != '\0'; i++)
-		;
-	for (j = 0; owner[j] != '\0'; j++)
-		;
-
-	nom = malloc(sizeof(char) * i + 1);
-	if (nom == NULL)
-	{
-		free(doge);
+	d = malloc(sizeof(dog_t));
+	if (!d)
 		return (NULL);
-	}
-	dueño = malloc(sizeof(char) * j + 1);
-	if (dueño == NULL)
-	{
-		free(nom);
-		free(doge);
-		return (NULL);
-	}
-	for (k = 0; k <= i; k++)
-		nom[k] = name[k];
-	for (k = 0; k <= j; k++)
-		dueño[k] = owner[k];
-
-	doge->name = nom;
-	doge->age = age;
-	doge->owner = dueño;
-
-	return (doge);
+	d->name = _strdup(name);
+	if (!(d->name))
+		return (free(d), NULL);
+	d->age = age;
+	d->owner = _strdup(owner);
+	if (!(d->owner))
+		return (free(d->name), free(d), NULL);
+	return (d);
 }
